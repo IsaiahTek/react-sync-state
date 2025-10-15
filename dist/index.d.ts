6fa135b2657ef212c9783e40cb0c0f6528eb0069
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-10-15 10:27:27 +0100

    Added index. files

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from "./types";
+export * from "./synq";
+export * from "./use_store";
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./types";
+export * from "./synq";
+export * from "./use_store";
+// export * from "./store";
