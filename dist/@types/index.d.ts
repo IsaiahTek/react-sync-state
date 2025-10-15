6c40ba787188c3f315d14d3af252556816ab25b5
Engr., Isaiah Pius <isaiahtech1@gmail.com>
2025-10-15 13:15:35 +0100

    modified export in index.ts and updated package.json to include types and exports

--- dist/@types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from "./types";
-export * from "./synq";
-export * from "./use_store";
-export { SynqStore } from "./synq_store";
-export { Store } from "./store";
-export { useStore, useServerSyncedStore } from "./use_store";
--- dist/@types/use_store.d.ts
+++
@@ -1,5 +0,0 @@
-import { Store, SynqStore } from "./types";
-export declare function useStore<T>(store: Store<T>): T;
-export declare function useServerSyncedStore<T extends {
-    id: string;
-}>(store: SynqStore<T>): T[];
